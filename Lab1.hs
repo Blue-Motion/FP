016be89d8566915a4b463ab58f49cf4372038d9f
Blue-Motion <a.offereins+github@student.rug.nl>
2014-09-20 15:41:27 +0200

    A try fo complete the first exercize, however, it still needs some work

--- Lab1.hs
+++ Lab1.hs
@@ -1,13 +1,32 @@
---Exersise 1
+All functions are intentionally written in prefix notation. One of the goals was to reduce the number of parenthesises, but haskell somehow still needs them at points it shouldn't
 
+--Exersize 1
+
+--a
 isPrime :: Integer -> Bool
 isPrime 1 = True
 isPrime 2 = True
-isPrime x = not (even x) && notElem True [divides x y | y <- [2..div x 2], odd y]
+isPrime x = (&&) (not (even x)) (notElem True [divides x y | y <- [2..div x 2], odd y])
 
 divides :: Integer -> Integer -> Bool
 divides x y = (&&) ((>) x y) ((==) 0 (mod x y))
 
 listPrimes :: Integer -> [Integer]
-listPrimes 2 = [2]
-listPrimes x = listPrimes (x - 1) ++ [x]
+listPrimes x = [y | y <- [2..x], isPrime y]
+
+--up to 2^15, the following is reasonably fast, avobe it takes forever.
+--b
+cntPrimes :: Integer -> Int
+cntPrimes x = length (listPrimes x)
+
+--c
+oddPspTO :: Integer -> Integer -> [Integer]
+oddPspTO a upb = [n | n <- [3,5..upb], a^(n-1) `mod` n == 1, not (isPrime n)]
+
+--not working properly yey
+expmod :: Integer -> Integer -> Integer -> Integer
+expmod a e n
+       | e == 1 = mod a n
+       | (>) a n = expmod (mod a n) e n
+       | even e = expmod ((*) a a) (div e a) n
+       | otherwise = a * expmod a ((-) e 1) n
