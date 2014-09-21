3fd7bfd67101171a3eaf3121734201da57b85cf2
Blue-Motion <a.offereins+github@student.rug.nl>
2014-09-21 22:47:31 +0200

    tried to reverse the order theorem, no significant time improvements, corrected spell errors

--- Lab1.hs
+++ Lab1.hs
@@ -1,18 +1,19 @@
 --All functions are intentionally written in prefix notation. One of the goals was to reduce the number of parenthesis, but haskell somehow still needs them at points it shouldn't
 
+
 --Exersize 1
 
 --a
 isPrime :: Integer -> Bool
 isPrime 1 = False
 isPrime 2 = True
-isPrime x = (&&) (not (even x)) (notElem True [divides x y | y <- [3,5..div x 2]])
+isPrime x = (&&) (odd x) (notElem True [divides x y | y <- [3,5..div x 2]])
 
 divides :: Integer -> Integer -> Bool
-divides x y = (&&) ((>) x y) ((==) 0 (mod x y))
+divides x y = (==) 0 (mod x y)
 
 listPrimes :: Integer -> [Integer]
-listPrimes x = [y | y <- [2..x], isPrime y]
+listPrimes x = 2:[y | y <- [3,5..x], isPrime y]
 
 --up to 2^15, the following is reasonably fast, above it takes forever.
 --b
@@ -27,19 +28,17 @@ oddPspTO a upb = [n | n <- [3,5..upb], a^(n-1) `mod` n == 1, not (isPrime n)]
 expmod :: Integer -> Integer -> Integer -> Integer
 expmod a e n
        | (==) e 1 = mod a n
---       | (>) a n = expmod (mod a n) e n
        | even e = expmod ((*) a a) (div e 2) n
---       | otherwise = (*) a (expmod  (mod ((*) a a) n) (div e 2) n)
---       | otherwise = a * expmod a ((-) e 1) n
        | otherwise = mod ((*) a (expmod a ((-) e 1) n)) n
 
-
 impOddPspTO :: Integer -> Integer -> [Integer]
-impOddPspTO a upb = [n | n <- [3,5..upb], expmod a (n-1) n == 1, not (isPrime n)]
+impOddPspTO a upb = [n | n <- [3,5..upb], (==) (expmod a ((-) n 1) n) 1, not (isPrime n)]
 
 ord1 :: Integer -> Integer -> Integer
 ord1 a p = ord a (a `mod` p) 1 p
      where ord a e k p = if e == 1 then k else ord a (a*e `mod` p) (k+1) p
 
 order :: Integer -> Integer -> Integer
-order a p = 
+
+factor :: Integer -> [Integer]
+factor n = [y | y <- listPrimes (div n 2), mod n y == 0]
