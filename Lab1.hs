02e908a9bec85d8f60ecb794abd2ffa7dc72265a
Blue-Motion <a.offereins+github@student.rug.nl>
2014-09-21 14:42:25 +0200

    fiddling around with primes

--- Lab1.hs
+++ Lab1.hs
@@ -1,12 +1,12 @@
-All functions are intentionally written in prefix notation. One of the goals was to reduce the number of parenthesises, but haskell somehow still needs them at points it shouldn't
+--All functions are intentionally written in prefix notation. One of the goals was to reduce the number of parenthesis, but haskell somehow still needs them at points it shouldn't
 
 --Exersize 1
 
 --a
 isPrime :: Integer -> Bool
-isPrime 1 = True
+isPrime 1 = False
 isPrime 2 = True
-isPrime x = (&&) (not (even x)) (notElem True [divides x y | y <- [2..div x 2], odd y])
+isPrime x = (&&) (not (even x)) (notElem True [divides x y | y <- [3,5..div x 2]])
 
 divides :: Integer -> Integer -> Bool
 divides x y = (&&) ((>) x y) ((==) 0 (mod x y))
@@ -14,7 +14,7 @@ divides x y = (&&) ((>) x y) ((==) 0 (mod x y))
 listPrimes :: Integer -> [Integer]
 listPrimes x = [y | y <- [2..x], isPrime y]
 
---up to 2^15, the following is reasonably fast, avobe it takes forever.
+--up to 2^15, the following is reasonably fast, above it takes forever.
 --b
 cntPrimes :: Integer -> Int
 cntPrimes x = length (listPrimes x)
@@ -23,10 +23,23 @@ cntPrimes x = length (listPrimes x)
 oddPspTO :: Integer -> Integer -> [Integer]
 oddPspTO a upb = [n | n <- [3,5..upb], a^(n-1) `mod` n == 1, not (isPrime n)]
 
---not working properly yey
+--works somehow
 expmod :: Integer -> Integer -> Integer -> Integer
 expmod a e n
-       | e == 1 = mod a n
-       | (>) a n = expmod (mod a n) e n
-       | even e = expmod ((*) a a) (div e a) n
-       | otherwise = a * expmod a ((-) e 1) n
+       | (==) e 1 = mod a n
+--       | (>) a n = expmod (mod a n) e n
+       | even e = expmod ((*) a a) (div e 2) n
+--       | otherwise = (*) a (expmod  (mod ((*) a a) n) (div e 2) n)
+--       | otherwise = a * expmod a ((-) e 1) n
+       | otherwise = mod ((*) a (expmod a ((-) e 1) n)) n
+
+
+impOddPspTO :: Integer -> Integer -> [Integer]
+impOddPspTO a upb = [n | n <- [3,5..upb], expmod a (n-1) n == 1, not (isPrime n)]
+
+ord1 :: Integer -> Integer -> Integer
+ord1 a p = ord a (a `mod` p) 1 p
+     where ord a e k p = if e == 1 then k else ord a (a*e `mod` p) (k+1) p
+
+order :: Integer -> Integer -> Integer
+order a p = 
