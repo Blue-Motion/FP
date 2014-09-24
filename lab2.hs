5bd7f9159e1720fdcdac4008179f8b6e22c929a2
rug-carretero <thomcarretero@gmail.com>
2014-09-24 16:57:27 +0200

    Small start exercise 2

---
+++ lab2.hs
@@ -0,0 +1,13 @@
+import Data.List
+-- Exercise 1
+
+-- Exercise 2
+mults :: Integer -> [Integer]
+mults n = [x * n | x <- [1..9]]
+--
+multiples :: [Integer] -> [Integer]
+multiples [] = []
+multiples (x:xs) = sort ((mults x) ++ (multiples xs))
+
+multsum :: Integer -> [Integer] -> Integer
+multsum n (x:xs)  = 
