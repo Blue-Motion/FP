846d5b7959d7899fdc830e155f3b7dcc0b8e69e1
rug-carretero <thomcarretero@gmail.com>
2014-10-02 13:03:03 +0200

    Schop

--- lab2.hs
+++ lab2.hs
@@ -5,9 +5,11 @@ import Data.List
 mults :: Integer -> [Integer]
 mults n = [x * n | x <- [1..]]
 --
+
+--beschouw de lijst van mults hierboven als element, gebruik een functie die van elk van de genereerde lijsten van x:xs telkens de eerste neemt en map die op de lijst van lijsten. gebruik dan foldr ++ op deze lijsten om van de lijst van lijsten een lijst van elementen te maken.			
 multiples :: [Integer] -> [Integer]
 multiples [] = []
-multiples (x:xs) = sort ((mults x) ++ (multiples xs))
+multiples (x:xs) = mults x : multiples xs
 
-multsum :: Integer -> [Integer] -> Integer
-multsum n (x:xs)  = 
+--multsum :: Integer -> [Integer] -> Integer
+--multsum n (x:xs)  = 
