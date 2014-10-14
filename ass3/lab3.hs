fe07a8ba9d71995c067e182d99874b7566d34043
rug-carretero <thomcarretero@gmail.com>
2014-10-14 12:42:19 +0200

    ex1

---
+++ ass3/lab3.hs
@@ -0,0 +1,15 @@
+import Expression
+import Types
+
+--ex1
+brac :: String -> String
+brac b = "(" ++ b ++ ")"
+
+instance Show Expr where
+	show (Var x) = x
+	show (Val x) = show x
+	show (e :+: e') = brac(show e ++ " + " ++ show e')
+	show (e :-: e') = brac(show e ++ " - " ++ show e')
+	show (e :*: e') = brac(show e ++ " * " ++ show e')
+	show (e :/: e') = brac(show e ++ " / " ++ show e')
+	show (e :%: e') = brac(show e ++ " % " ++ show e')
