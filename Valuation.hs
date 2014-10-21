0c6c0ed2f923952445e13c1f588fe01d946be814
Blue-Motion <a.offereins+github@student.rug.nl>
2014-10-21 12:04:04 +0200

    valuation before cleanup

--- Valuation.hs
+++ Valuation.hs
@@ -9,7 +9,10 @@ type Valuation = [(Name, Integer)]
 --list comprehension to the max :)
 
 valuations :: [(Name,Domain)] -> [Valuation]
-valuations [] = []
+valuations [] = [[]]
+valuations ((x,y):[]) =  [[(x,v)] | v <- y]
 --valuations ((x,y):xs) = [(x,v) | v <- y]:(valuations xs)
 --valuations xs = [[((fst n),v) | v <- (snd n)] | n <- xs] 
-valuations xs = [[((map fst xs),v) | v <- (concat (map snd xs))] | n <- xs] 
+--valuations xs = [[((map fst xs),v) | v <- (concat (map snd xs))] | n <- xs] 
+valuations (x:xs) = concat [(map (y++) (valuations xs)) | y <- (vals x)]
+	   where vals (x,y) =  [[(x,v)] | v <- y]
