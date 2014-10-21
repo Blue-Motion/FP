e51fdca2fba071c00e78308cb81d133d68f94be5
Blue-Motion <a.offereins+github@student.rug.nl>
2014-10-21 13:43:48 +0200

    cleanup of cvaluation, some fiddling with simplifieing

--- Expression.hs
+++ Expression.hs
@@ -2,6 +2,7 @@ module Expression where
 
 import Types
 import Data.List
+import Valuation
 
 data Expr =
     Val Integer
@@ -28,9 +29,33 @@ vars = nub . sort . var
   where
   var (Val a) = []
   var (Var a) = [a]
+--cant the statements below be becombined?
+--  var (a -> b) = var a ++ var b
+
+
   var (a :+: b) = var a ++ var b
   var (a :-: b) = var a ++ var b
   var (a :*: b) = var a ++ var b
   var (a :/: b) = var a ++ var b
   var (a :%: b) = var a ++ var b
+--      where f = (Expr -> Expr -> Expr)  
+
+evalExpr :: Expr -> Valuation -> Integer
+evalExpr e v
+	 | ((length . vars) e) > (length v) = error "not enough valuated parameters"
+	 | otherwise = 0
+
+--this cant be the right way, this takes >9000 patterns
+simplifyExpr :: Expr -> Expr
+simplifyExpr ((Val a) :+: (Val b)) = Val (a + b)
+simplifyExpr ((Val a) :-: (Val b)) = Val (a - b)
+simplifyExpr ((Val a) :*: (Val b)) = Val (a * b)
+simplifyExpr ((Val a) :/: (Val b)) = Val (div a b)
+simplifyExpr ((Val a) :%: (Val b)) = Val (mod a b)
+simplifyExpr ((Var a) :+: b) = Var a :+: simplifyExpr b
+simplifyExpr ((Var a) :-: b) = Var a :-: simplifyExpr b
+simplifyExpr ((Var a) :*: b) = Var a :*: simplifyExpr b
+simplifyExpr ((Var a) :/: b) = Var a :/: simplifyExpr b
+simplifyExpr ((Var a) :%: b) = Var a :%: simplifyExpr b
+simplifyExpr a = a
 
